9a22cf93d4c83d81e36f31111057df0455853a10
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-01-30 23:14:32 +0600

    require auth modify

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import { Navigate, useLocation, } from 'react-router-dom';
 import { UserContext } from '../../App';
 const RequireAuth = ({ children, ...rest }) => {
-
     const [loggedInUser] = useContext(UserContext);
     const auth = loggedInUser.email;
     const location = useLocation();
