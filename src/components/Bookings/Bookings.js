59678bf00b1a3f9d02f1d1903161c32cee4da919
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-13 21:10:50 +0600

    authorization verified

--- src/components/Bookings/Bookings.js
+++ src/components/Bookings/Bookings.js
@@ -12,21 +12,17 @@ const Bookings = () => {
                     'Content-Type': 'application/json',
                     authorization: `Bearer ${sessionStorage.getItem('token')}`,
                 },
-
             }
-
         )
             .then(res => res.json())
             .then(data => setBooking(data))
     }, [])
-
     return (
         <div>
             <h3>You Have {bookings.length} bookings</h3>
             {
                 bookings.map((book) => <li key={Math.random().toString(36).substr(2, 9)}>
                     {book.name}, From: {book.checkIn} To: {book.checkOut} </li>)
-
             }
         </div>
     );
