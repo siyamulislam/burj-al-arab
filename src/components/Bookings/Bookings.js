27bfca4aad713c1cfd355699863247a1253e38bb
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-13 19:24:01 +0600

    dynamic query email

--- src/components/Bookings/Bookings.js
+++ src/components/Bookings/Bookings.js
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../App';
 
 const Bookings = () => {
     const [bookings,setBooking]=useState([])
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     useEffect(()=>{
-        fetch('http://localhost:5000/bookings')
+        fetch('http://localhost:5000/bookings?email='+loggedInUser.email)
         .then(res=>res.json())
         .then(data=>setBooking(data))
     },[])
+    
     return (
         <div>
             <h3>You Have {bookings.length} bookings</h3>
             {
-                bookings.map((book )=> <li key={Math.random().toString(36).substr(2, 9)}>{book.name} From: {book.checkIn} To: {book.checkOut} </li>)
+                bookings.map((book )=> <li key={Math.random().toString(36).substr(2, 9)}>
+                    {book.name}, From: { book.checkIn} To: {book.checkOut} </li>)
+               
             }
         </div>
     );
